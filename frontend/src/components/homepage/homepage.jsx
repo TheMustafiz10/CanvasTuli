24e7d2929c8ea2da978ab3b3c657dc2ba88ba343
MD Mustafizur Rahman <rahmanmdmustafizur961@gmail.com>
2026-09-01 18:17:14 +0600

    Update homepage.jsx

--- frontend/src/components/homepage/homepage.jsx
+++ frontend/src/components/homepage/homepage.jsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Footer from './Footer';
+import Footer from './footer';
 import './CSS/homepage.css';
 
 // Dummy artwork data with online images
