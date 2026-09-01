6750df62523b3220b42a466e047ebab643f14029
MD Mustafizur Rahman <rahmanmdmustafizur961@gmail.com>
2026-09-01 17:25:36 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -12,8 +11,16 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (
+              id.includes('react') ||
+              id.includes('react-dom') ||
+              id.includes('react-router-dom')
+            ) {
+              return 'vendor'
+            }
+          }
         },
       },
     },
@@ -21,4 +28,4 @@ export default defineConfig({
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
   },
-});
+})
